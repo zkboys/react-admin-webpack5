5c248ced03076f41a37c07891fa8d99de2d45be6
zkboys <876213774@qq.com>
2022-10-26 15:14:39 +0800

    :art: 整理代码

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,28 +1,21 @@
-import { Button } from 'antd';
-import { PageContent } from '@ra-lib/adm';
+import {useState} from 'react';
+import {PageContent, DatePickerLong} from '@ra-lib/adm';
 import config from 'src/commons/config-hoc';
 import s from './style.module.less';
 
 export default config({
-    // title: '首页', // 注释掉，不生成菜单
     auth: false,
-    connect: (state) => {
-        return {
-            user: state.demo.user,
-        };
-    },
 })(function Index(props) {
-    const { user } = props;
-    console.log('redux示例：', user);
+    const [date, setDate] = useState();
+    console.log(date);
     return (
         <PageContent className={s.root}>
             <h1>首页</h1>
-            <Button
-                r-code="xxxxxx"
-                onClick={() => props.action.demo.getUser()}
-            >
-                获取用户
-            </Button>
+            <DatePickerLong
+                value={date}
+                onChange={setDate}
+                longValue="long"
+            />
         </PageContent>
     );
 });
